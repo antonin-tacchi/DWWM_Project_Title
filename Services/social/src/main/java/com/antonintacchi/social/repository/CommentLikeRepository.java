808ea2f78ca8e9e8a9320df5ea1af9f65b2f931f
Antonin Tacchi <antonin.tacchi@laplateforme.io>
2026-05-12 15:01:41 +0200

    feat(social): update CommentLikeRepository to use @Transactional annotation and change social service port to 9010

--- Services/social/src/main/java/com/antonintacchi/social/repository/CommentLikeRepository.java
+++ Services/social/src/main/java/com/antonintacchi/social/repository/CommentLikeRepository.java
@@ -2,10 +2,12 @@ package com.antonintacchi.social.repository;
 
 import com.antonintacchi.social.entity.CommentLike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
 
     Boolean existsByUserIdAndCommentId(Long userId, Long commentId);
+    @Transactional
     void deleteByUserIdAndCommentId(Long userId, Long commentId);
 
 }
